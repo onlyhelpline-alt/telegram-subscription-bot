bd73eda0c647e149ea14c5ac7875c1b1c460067e
onlyhelpline-alt <onlyhelpline@gmail.com>
2026-03-21 09:00:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+ADMIN_USERNAME = "ckg2754"  # change if needed
+UPI_ID = "bestcourseseller@ybl"
