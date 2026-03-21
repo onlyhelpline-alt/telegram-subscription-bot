1366216d5a80fc57261ab86e5620a96847a8e281
onlyhelpline-alt <onlyhelpline@gmail.com>
2026-03-21 10:28:06 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,4 @@
 import psycopg2
-from datetime import datetime
 from config import DATABASE_URL
 
 conn = psycopg2.connect(DATABASE_URL)
@@ -16,9 +15,20 @@ def init_db():
         expiry_date TEXT
     )
     """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS plans (
+        id SERIAL PRIMARY KEY,
+        name TEXT,
+        price INT,
+        validity INT
+    )
+    """)
+
     conn.commit()
 
 
+# ---------- USERS ----------
 def add_user(uid, username, plan, price, join, expiry):
     cursor.execute(
         "INSERT INTO users VALUES (%s,%s,%s,%s,%s,%s)",
@@ -26,12 +36,23 @@ def add_user(uid, username, plan, price, join, expiry):
     )
     conn.commit()
 
+def get_users():
+    cursor.execute("SELECT * FROM users")
+    return cursor.fetchall()
 
 def remove_user(uid):
     cursor.execute("DELETE FROM users WHERE user_id=%s", (uid,))
     conn.commit()
 
 
-def get_users():
-    cursor.execute("SELECT * FROM users")
+# ---------- PLANS ----------
+def add_plan(name, price, validity):
+    cursor.execute(
+        "INSERT INTO plans (name, price, validity) VALUES (%s,%s,%s)",
+        (name, price, validity)
+    )
+    conn.commit()
+
+def get_plans():
+    cursor.execute("SELECT * FROM plans")
     return cursor.fetchall()
